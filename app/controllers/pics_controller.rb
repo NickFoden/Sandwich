964b64d949e0979748d92f5dada765e2ca66d051
Nick Foden <nickfoden@gmail.com>
2016-10-20 17:28:36 -0400

    Put order back to descending

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -4,7 +4,7 @@ class PicsController < ApplicationController
 	before_action :authenticate_user!, except: [:index, :show]
 
 	def index
-		@pics = Pic.all.order("created_at asc")
+		@pics = Pic.all.order("created_at DESC")
 	end
 
 	def show
